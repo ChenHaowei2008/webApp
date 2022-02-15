455385299e0901b1a9bbccae691a03cf7134a204
ChenHaowei2008 <haoweichenchw@gmail.com>
2022-02-15 18:31:30 +0800

    we do a little trolling

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
+app.get('/', (req, res) => {
+	res.send("your ip has been logged:)")
+})
 app.listen(PORT)
