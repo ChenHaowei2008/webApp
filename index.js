1d123f6197721f74a41524444197bcd977adc9ab
ChenHaowei2008 <haoweichenchw@gmail.com>
2022-02-17 22:02:41 +0800

    Did nothing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 const fs = require('fs')
 const app = express()
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + "/pages/" + req.originalUrl)
+	if(!fs.existsSync(__dirname + "/pages" + req.originalUrl)) return res.send(`The page ${__dirname + "/pages" + req.originalUrl} does not exist`)
+	res.sendFile(__dirname + "/pages" + req.originalUrl)
 })
 app.listen(PORT)
