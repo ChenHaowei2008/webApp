50774d46c656c3fa4006c608c3a6db187370f160
ChenHaowei2008 <haoweichenchw@gmail.com>
2022-02-17 21:52:13 +0800

    added stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const fs = require('fs')
 const app = express()
-app.get('/', (req, res) => {
-	res.send("your ip has been logged:)")
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + "/pages/" + req.originalUrl)
 })
 app.listen(PORT)
